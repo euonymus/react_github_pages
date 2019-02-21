364e5657725f103f815b15ca3b9c55205d63a6fa
euonymus <euonymus0220@gmail.com>
2019-02-21 19:21:49 +0900

    Updates

---
+++ precache-manifest.3271220394bf179937370bb23705529b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_github_pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42f07e26c63e02ff6e50",
+    "url": "/react_github_pages/static/js/runtime~main.42f07e26.js"
+  },
+  {
+    "revision": "90d6e72037f91aab9cd7",
+    "url": "/react_github_pages/static/js/main.90d6e720.chunk.js"
+  },
+  {
+    "revision": "efab772cd087ef7ead4a",
+    "url": "/react_github_pages/static/js/2.efab772c.chunk.js"
+  },
+  {
+    "revision": "90d6e72037f91aab9cd7",
+    "url": "/react_github_pages/static/css/main.0a12c687.chunk.css"
+  },
+  {
+    "revision": "e7e143b3c5ea120831f5142f43599e3c",
+    "url": "/react_github_pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fedb0b73e04984ea96b65ab1e92b25f.js"
+  "/react_github_pages/precache-manifest.3271220394bf179937370bb23705529b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react_github_pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
